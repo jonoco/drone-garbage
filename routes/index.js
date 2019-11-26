6c8b762c9ffe5daf8ae2979dcf758211da06e943
Joshua Cox <joshua.n.cox@gmail.com>
2019-11-26 09:29:38 +0000

    session bug on index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Drone Garbage', user: req.session.user.firstName });
+    let name
+    if (req.session.user) {
+        name = req.session.user.firstName
+    }
+
+    res.render('index', { title: 'Drone Garbage', user: name });
 });
 
 module.exports = router;
