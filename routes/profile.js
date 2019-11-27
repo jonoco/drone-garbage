6203b3f8c0a9ba1751694b42517b47a6a8cd6338
Lonmarg <hammong@oregonstate.edu>
2019-11-27 21:35:13 +0000

    Removed user links from index / Added editable user bio to profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,23 +2,79 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
 
-router.get('/:user', function(req, res, next) {
+router.get('/:user', function(req, res, next) 
+{
     const username = req.params.user
     
-    User.findOne({ where: { username: username }})
-        .then(function(user) {
-            if (!user) {
-                res.status(404)
-                res.redirect('/')
-            } else {
-                res.render('profile', {
-                    user: user
-                })
-            }
+    User.findOne({ where: { username: username }}).then(function(user) 
+    {
+        if (!user) 
+        {
+            res.status(404)
+            res.redirect('/')
+        } 
+        else 
+        {
+            res.render('profile', { title: 'Drone Garbage', user: user })
+        }
             
-        }).catch(function(error) {
+        }).catch(function(error) 
+        {
             res.send(error)
         })
+})
+
+router.get('/', function(req, res, next) 
+{
+    let user
+    if (req.session.user) 
+    {
+        user = req.session.user
+    }
+
+    res.render('profile', { title: 'Drone Garbage', user });
+})
+
+router.post('/', function(req, res, next) 
+{
+    let curUser
+    if (req.session.user) 
+    {
+        curUser = req.session.user
+    
+        if(req.body.btn == 'submitPhoto') 
+        {
+            res.render('profile', { title: 'PHOTO UPDATED', curUser})
+        }
+        else if(req.body.btn == 'submitBio')
+        {
+            //Update the users Bio to match what the user has typed into the userBio textbox
+            User.update({bio: req.body.userBio},{where: {username: curUser.username}}).then(function(tmp)
+            {
+                //.then returns once update is completed
+                req.session.user.bio = req.body.userBio
+                curUser.bio = req.body.userBio
+
+                //Update the sessions with the updated user
+                User.findOne({where: { username: curUser.username }}).then(function(user) 
+                {
+                    if (!user) 
+                    {
+                        res.status(400)
+                        res.render('login', 
+                        {
+                            error: { nouser: 'username not found' }
+                        })
+                    } 
+                    else 
+                    {
+                        req.session.user = user
+                        res.render('profile', { title: 'Drone Garbage', user})
+                    }
+                })
+            })
+        }  
+    }  
 })
 
 
